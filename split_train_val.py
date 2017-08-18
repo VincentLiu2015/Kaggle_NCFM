44cbe17d9d8784fa02c163ecf0612053ed6ac74a
VincentLiu <VincentLiu2015@users.noreply.github.com>
2017-08-18 18:13:48 +0800

    Update split_train_val.py
    add info explain

--- split_train_val.py
+++ split_train_val.py
@@ -20,12 +20,15 @@ split_proportion = 0.8
 for fish in FishNames:
     if fish not in os.listdir(root_train):
         os.mkdir(os.path.join(root_train, fish))
-
+#http://www.cnblogs.com/niocai/p/3351587.html   
+#http://www.runoob.com/python/att-string-join.html
+#http://www.cnblogs.com/luhouxiang/archive/2012/05/22/2514081.html
     total_images = os.listdir(os.path.join(root_total, fish))
 
     nbr_train = int(len(total_images) * split_proportion)
-
+#http://www.runoob.com/python/func-number-shuffle.html
     np.random.shuffle(total_images)
+#range   http://www.cnblogs.com/buro79xxd/archive/2011/05/23/2054493.html
 
     train_images = total_images[:nbr_train]
 
@@ -34,6 +37,7 @@ for fish in FishNames:
     for img in train_images:
         source = os.path.join(root_total, fish, img)
         target = os.path.join(root_train, fish, img)
+    #http://blog.csdn.net/xmnathan/article/details/36217631
         shutil.copy(source, target)
         nbr_train_samples += 1
 
